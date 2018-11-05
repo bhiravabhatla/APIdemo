29b5b4eec790e7ced8fb99094f352302f1479361
bsantosh <bhiravabhatla@gmail.com>
2018-11-05 17:03:58 +0530

    @santosh : Refactors code to use Service layer

--- src/main/java/org/tw/demo/account/controllers/AccountController.java
+++ src/main/java/org/tw/demo/account/controllers/AccountController.java
@@ -1,56 +1,39 @@
 package org.tw.demo.account.controllers;
 
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.tw.demo.account.commands.AccountCommand;
 import org.tw.demo.account.domains.Account;
-import org.tw.demo.account.repositories.AccountRepository;
-import org.tw.demo.user.commands.UserCommand;
+import org.tw.demo.account.services.AccountService;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 
-    @Autowired
-    AccountRepository accountRepository;
-    public static final String URL ="http://localhost:6060/user/create";
 
+    private AccountService accountService;
 
-
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @PostMapping("/create")
     public Map<String,String> createAccount(@RequestBody AccountCommand accountCommand){
 
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String,String> response = new HashMap<>();
-        UserCommand userCommand=new UserCommand(accountCommand.getFullName(),accountCommand.getPhone());
-        JsonNode userResponse =  restTemplate.postForObject(URL,userCommand,JsonNode.class);
-        System.out.println(userResponse.get("id").asText());
-        String accountId= UUID.randomUUID().toString();
-
-        response.put("account_id",accountId);
-        response.put("user_id",userResponse.get("id").asText());
-        accountRepository.save(new Account(accountId,userResponse.get("id").asText(),accountCommand.getEmail(),accountCommand.getDesc()));
-        return  response;
-
+        return  accountService.createAccount(accountCommand);
 
     }
 
     @GetMapping("/")
     public List<Account> getAllaccounts(){
-        return accountRepository.findAll();
+        return accountService.getAllaccounts();
     }
 
     @GetMapping("/{id}")
     public Account getAccountDetails(@PathVariable String id){
-       return accountRepository.findByAccountId(id);
+       return accountService.getAccountDetails(id);
     }
 }
--- src/main/java/org/tw/demo/user/controllers/UserController.java
+++ src/main/java/org/tw/demo/user/controllers/UserController.java
@@ -1,43 +1,37 @@
 package org.tw.demo.user.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.tw.demo.user.commands.UserCommand;
 import org.tw.demo.user.domains.User;
-import org.tw.demo.user.repositories.UserRepository;
+import org.tw.demo.user.services.UserService;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/create")
     public Map<String,String> createUser(@RequestBody UserCommand userCommand){
-        String  userID=UUID.randomUUID().toString();
-        Map<String,String> result=new HashMap<>();
-        result.put("id",userID);
-        System.out.println(userCommand.getFullName());
-        userRepository.save(new User(userID,userCommand.getFullName(),userCommand.getPhone()));
-        return result;
+        return userService.createUser(userCommand);
 
     }
 
     @GetMapping("/")
     public List<User> getAllUsers(){
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable String id){
 
-        return userRepository.findByUserID(id);
+        return userService.getUserById(id);
     }
 }
